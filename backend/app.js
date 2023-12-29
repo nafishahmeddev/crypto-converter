7769e82e82e08be25fefc9a18fa8f937b7dbdead
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2023-12-29 07:03:33 +0530

    [UPDATE] Removed cors origin

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const { CurrencyRouter } = require("./routes");
 const app = new express();
 
 //register middleware
-app.use(cors());
+// app.use(cors());
 app.use(express.json({}));
 app.use(express.urlencoded({ extended: true }));
 
